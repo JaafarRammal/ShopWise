aaeac95800e5ee15fdf0b475f86520c2d3ff31fb
Jaafar Rammal <jaafarrammal@dyn3157-204.wlan.ic.ac.uk>
2020-02-08 18:07:06 +0000

    Barcode working

---
+++ iShop/iShop/Controller/CameraController.swift
@@ -0,0 +1,93 @@
+//
+//  QRScannerViewController.swift
+//  QRCodeReader
+//
+//  Created by KM, Abhilash a on 08/03/19.
+//  Copyright © 2019 KM, Abhilash. All rights reserved.
+//
+
+import UIKit
+
+class QRScannerViewController: UIViewController {
+    
+    let defaults = UserDefaults.standard
+    var attendees = [String]()
+    
+    @IBOutlet weak var scannerView: QRScannerView! {
+        didSet {
+            scannerView.delegate = self
+        }
+    }
+    @IBOutlet weak var scanButton: UIButton! {
+        didSet {
+            scanButton.setTitle("STOP", for: .normal)
+        }
+    }
+    
+    var qrData: QRData? = nil {
+        didSet {
+            if qrData != nil {
+                print(qrData!)
+//                let code = String(qrData!.codeString!.split(separator: "-")[1])
+                scannerView.stopScanning()
+                }
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+ 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
+        if !scannerView.isRunning {
+            scannerView.startScanning()
+        }
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        if !scannerView.isRunning {
+            scannerView.stopScanning()
+        }
+    }
+
+    @IBAction func scanButtonAction(_ sender: UIButton) {
+        scannerView.isRunning ? scannerView.stopScanning() : scannerView.startScanning()
+        let buttonTitle = scannerView.isRunning ? "STOP" : "SCAN"
+        sender.setTitle(buttonTitle, for: .normal)
+    }
+}
+
+
+extension QRScannerViewController: QRScannerViewDelegate {
+    func qrScanningDidStop() {
+        let buttonTitle = scannerView.isRunning ? "STOP" : "SCAN"
+        scanButton.setTitle(buttonTitle, for: .normal)
+    }
+    
+    func qrScanningDidFail() {
+        presentAlert(withTitle: "Error", message: "Scanning Failed. Please try again")
+    }
+    
+    func qrScanningSucceededWithCode(_ str: String?) {
+        self.qrData = QRData(codeString: str)
+    }
+    
+    
+    
+}
+
+func isValid(data: String, compare: [String]) -> Bool {
+    if(compare.contains(data)){
+        return true
+    }
+    return false
+}
+
+func isKeyPresentInUserDefaults(key: String) -> Bool {
+    return UserDefaults.standard.object(forKey: key) != nil
+}
