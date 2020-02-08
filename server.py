43d80c3288721d70039fb5732716a8c05fafac04
Arjun Banerjee <ab5518@ic.ac.uk>
2020-02-08 18:16:37 +0000

    still fixing:

--- server.py
+++ server.py
@@ -3,7 +3,7 @@ from flask import Flask, request, jsonify
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
-@app.route('/hello', methods=['GET'])
+@app.route('/', methods=['GET'])
 def hello_world():
     return 'Hey, we have Flask in a Docker container!'
 
