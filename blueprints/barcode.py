1e69002586847ff5e1b81180e945cfae9bac5987
Prabhjot Singh Grewal <prab99k@gmail.com>
2020-02-08 22:15:25 +0000

    new api calls

--- blueprints/barcode.py
+++ blueprints/barcode.py
@@ -1,50 +1,69 @@
 import json
-
 from flask import Blueprint, Response, request
+import requests
 import http.client, urllib.request, urllib.parse, urllib.error, base64
 
 barcode = Blueprint('barcode', __name__)
 
-food_db_key = "ad08b45030aa9122d39cbb6efb154f87"
-food_db_id = "78938bf0"
+# food_db_key = "ad08b45030aa9122d39cbb6efb154f87"
+# food_db_id = "78938bf0"
+
+eanToken = 'cee7b3d1236f7076c16d0f9a02d4e5'
 
 # headers = {
-#     # Request headers
-#     'Ocp-Apim-Subscription-Key': '{' + food_db_key + '}',
+#   'Content-Type': 'application/json',
+#   'Accept': 'application/json'
 # }
+
+test = """[{"ean":"7622210740489","name":"Belvita Milk and Cereal Biscuits 12 Pack 540G","categoryId":"0","categoryName":"Unknown"}] """
 
 class Product:
     def __init__(self, upc, name, price, trace):
         self.upc = upc
         self.name = name
 
-@barcode.route('/upc/<id>')
+@barcode.route('/ean/<id>')
 def get_product(id):
 
-    product = getInfo(id)
-    obj = json.load(product)
-    upc = obj['text'][5:-1]
-    print(upc)
+    # product = getInfo(id)
+    product = test
+    obj = json.loads(product)
+    name = obj[0].get("name")
+    # print(obj['name'])
+    price, imageURL = getPrice(name)
 
-    parsed = Product(upc, "")
+    # parsed = Product(upc, "")
 
     return product
 
-def getInfo(upc):
-    params = urllib.parse.urlencode({
-        # Request parameters
-        'upc': '{' + upc + '}',
-        'app_id': food_db_id,
-        'app_key': food_db_key
-    })
-
+def getInfo(ean):
+    # params = urllib.parse.urlencode({
+    #     # Request parameters
+    #     'upc': '{' + upc + '}',
+    #     'app_id': food_db_id,
+    #     'app_key': food_db_key
+    # })
+    params = 'token=' + eanToken + '&op=barcode-lookup&format=json&ean='+ean
     try:
-        conn = http.client.HTTPSConnection('api.edamam.com')
-        conn.request("GET", "/api/food-database/parser?%s" % params)
+        conn = http.client.HTTPSConnection('api.ean-search.org')
+        conn.request("GET", "/api?%s" % params)
         response = conn.getresponse()
         data = response.read()
-        print(data)
         conn.close()
         return data
     except Exception as e:
         print("[Errno {0}] {1}".format(e.errno, e.strerror))
+
+def getPrice(name):
+    headers = {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+    }
+    name.replace(" ", "%20")
+    resp = requests.get('https://api.upcitemdb.com/prod/trial/search?s='+name, headers=headers)
+    res = json.loads(resp.text)['items'][0]
+    price = res['offers'][0]['price']
+    imageURL = res['images'][0]
+    print(price)
+    print(imageURL)
+    return price, imageURL
