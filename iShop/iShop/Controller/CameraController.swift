5347022b9393407791c2bc32550709fe4224cd9b
Jaafar Rammal <jaafarrammal@Sableur-Mac.local>
2020-02-09 00:15:14 +0000

    Front cam working

--- iShop/iShop/Controller/CameraController.swift
+++ iShop/iShop/Controller/CameraController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class QRScannerViewController: UIViewController {
     
+    struct Item {
+        var name: String
+        var price: String
+        var image: UIImage?
+    }
  
     @IBOutlet weak var addToCartButton: UIButton!
     @IBOutlet weak var alternativesButton: UIButton!
@@ -18,6 +23,9 @@ class QRScannerViewController: UIViewController {
     @IBOutlet weak var itemImage: UIImageView!
     @IBOutlet weak var itemName: UILabel!
     @IBOutlet weak var itemPrice: UILabel!
+    @IBOutlet weak var message: UILabel!
+    
+    @IBOutlet weak var cartButton: UIButton!
     
     @IBOutlet weak var scannerView: QRScannerView! {
         didSet {
@@ -25,7 +33,6 @@ class QRScannerViewController: UIViewController {
         }
     }
     @IBOutlet weak var scanButton: UIButton!
-    @IBOutlet weak var scanButtonIcon: UIImageView!
     var qrData: QRData? = nil {
         didSet {
             if qrData != nil {
@@ -47,6 +54,7 @@ class QRScannerViewController: UIViewController {
         if !scannerView.isRunning {
             scannerView.stopScanning()
         }
+        message.alpha = 0
     }
     
     override func viewWillDisappear(_ animated: Bool) {
@@ -58,18 +66,32 @@ class QRScannerViewController: UIViewController {
 
     @IBAction func scanButtonAction(_ sender: UIButton) {
         scannerView.startScanning()
+        print("bye?")
 //        HTTPsendRequest()
+    }
+    
+    @IBAction func loadCartView(_ sender: UIImageView) {
+        print("hi?")
+        let storyBoard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+        let newViewController = storyBoard.instantiateViewController(withIdentifier: "CartController") as! CartController
+        newViewController.modalPresentationStyle = .fullScreen
         
+        let transition = CATransition()
+        transition.duration = 0.2
+        transition.type = CATransitionType.push
+        transition.subtype = CATransitionSubtype.fromLeft
+        transition.timingFunction = CAMediaTimingFunction(name:CAMediaTimingFunctionName.easeInEaseOut)
+        view.window!.layer.add(transition, forKey: kCATransition)
+       self.present(newViewController, animated: false, completion: nil)
     }
     
     func displayItem(code: String){
         print(code)
-        self.scanButton.isHidden = true
-        self.scanButtonIcon.isHidden = true
         self.scannerView.addSubview(self.itemview)
         self.addToCartButton.layer.cornerRadius = 20
         self.alternativesButton.layer.cornerRadius = 20
         self.itemview.center = self.view.center
+        self.itemview.center.y -= 70
         self.itemview.layer.cornerRadius = 25
         view.bringSubviewToFront(itemview)
         self.itemName.text = "Item Name!"
@@ -78,14 +100,34 @@ class QRScannerViewController: UIViewController {
     
     @IBAction func addToCart(_ sender: Any) {
         itemview.removeFromSuperview()
-        self.scanButton.isHidden = false
-        self.scanButtonIcon.isHidden = false
+        let a = Item(name: "HI", price: "3")
+        addItemToCart(item: a)
     }
     
     @IBAction func alternatives(_ sender: Any) {
         itemview.removeFromSuperview()
-        self.scanButton.isHidden = false
-        self.scanButtonIcon.isHidden = false
+    }
+    
+    func addItemToCart(item: Item){
+        fadeViewInThenOut(label: message, delay: TimeInterval(3))
+    }
+    
+    func fadeViewInThenOut(label : UILabel, delay: TimeInterval) {
+
+        let animationDuration = 0.25
+
+        // Fade in the view
+        UIView.animate(withDuration: animationDuration, animations: { () -> Void in
+            label.alpha = 1
+            }) { (Bool) -> Void in
+
+                // After the animation completes, fade out the view after a delay
+
+                UIView.animate(withDuration: animationDuration, delay: delay, options: .curveEaseInOut, animations: { () -> Void in
+                    label.alpha = 0
+                    },
+                    completion: nil)
+        }
     }
 }
 
---
+++ iShop/iShop/Controller/CartController.swift
@@ -0,0 +1,14 @@
+//
+//  CartController.swift
+//  iShop
+//
+//  Created by Jaafar Rammal on 2/8/20.
+//  Copyright © 2020 Jaafar Rammal. All rights reserved.
+//
+
+import UIKit
+
+class CartController: UIViewController {
+
+}
+
