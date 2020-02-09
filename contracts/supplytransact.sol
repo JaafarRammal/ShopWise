fb267add85d0ad9ce771da1ae36c83092d272362
Arjun Banerjee <ab5518@ic.ac.uk>
2020-02-09 02:18:33 +0000

    Working smart contract

--- contracts/supplytransact.sol
+++ contracts/supplytransact.sol
@@ -1,19 +1,54 @@
 pragma solidity ^0.6.0;
+pragma experimental ABIEncoderV2;
 
 contract SupplyChain {
     address owner;
-    string public origin;
-    string public destination;
-    string public seller;
-    string public role;
-    string public transport;
 
-    constructor(string memory _origin, string memory _destination, string memory _seller, string memory _role, string memory _transport) public {
-        origin = _origin;
-        destination = _destination;
-        seller = _seller;
-        role = _role;
-        transport = _transport;
+    struct TraceEntry {
+        address seller_address;
+        string origin;
+        string seller;
+        string role;
+        string transport;
+        uint256 carbon_footprint;
+    }
+
+    struct Trace {
+        uint256 id;
+        TraceEntry[] steps;
+    }
+
+
+    Trace[100] public products ;
+    uint256 public productLength = 0;
+
+    function getProducts() public view returns( Trace[100] memory) {
+        return products;
+    }
+
+    function getProduct(uint256 _id) public view returns (Trace memory ){
+        for (uint256 i = 0; i < products.length; ++i) {
+            if(products[i].id == _id) {
+                return products[i];
+            }
+        }
+    }
+
+    function transfer(uint256 _id, string memory _origin, string memory _seller, string memory _role, string memory _transport, uint256 _carbon_footprint) public {
+        // products(_id) = Trace(_id, [Trace(msg.sender, _origin, _seller, _role, _transport, _carbon_foortprint)]);
+        for (uint256 i = 0; i < products.length; ++i) {
+            if(products[i].id == _id) {
+                products[i].steps.push(TraceEntry(msg.sender, _origin, _seller, _role, _transport, _carbon_footprint));
+                return;
+            }
+        }
+        Trace storage trace = products[productLength];
+        trace.id = _id;
+        trace.steps.push(TraceEntry(msg.sender, _origin, _seller, _role, _transport, _carbon_footprint));
+    }
+
+    constructor() public {
         owner = msg.sender;
     }
+
 }
